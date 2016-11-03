629601298656176b5860c30c4f7506cc8bc5fe4e
Squidswell <betidswell@vassar.edu>
2016-11-03 13:48:22 -0400

    Added Beacon Test
    Also rebuilt folders to make them usable

---
+++ Find Beacon/GripTest 2.c
@@ -0,0 +1,59 @@
+int LMOTOR = 0;
+int RMOTOR = 3;
+int LSERVO = 2;
+int RSERVO = 0;
+int NBEACON= 10;
+
+void forward(){
+	motor(LMOTOR, 50);
+	motor(RMOTOR, 50);
+	sleep(30);
+}
+
+void turnLeft(){
+	motor(LMOTOR, -50);
+	motor(RMOTOR, 50);
+	sleep(50);
+}
+
+void turnRight(){
+	motor(LMOTOR, 50);
+	motor(RMOTOR, -50);
+	sleep(50);
+}
+
+void turnAround(){
+	motor(LMOTOR, -50);
+	motor(RMOTOR, 50);
+	sleep(100);
+}
+
+void manipulate(){
+	set_servo_position(LSERVO, 1200);
+	set_set_position(RSERVO, 1100);
+}
+
+void findBeacon(){
+	if(NBEACON < 100){
+		forward();
+	}
+	else if(EBEACON < 100){
+		turnRight();
+	}
+	else if(WBEACON < 100){
+		turnLeft();
+	}
+	else if(SBEACON < 100{
+		turnAround();
+	}
+	else{
+		forward();
+	}
+}
+
+int main(){
+	while(1){
+		findBeacon();
+	}
+}
+		
