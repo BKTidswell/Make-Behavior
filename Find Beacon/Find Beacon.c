1a487029db6150c81ad15586fb607db65a23669d
Squidswell <betidswell@vassar.edu>
2016-11-03 14:14:05 -0400

    Beautiful variable delcarations
    Try using this as a base

--- Find Beacon/Find Beacon.c
+++ Find Beacon/Find Beacon.c
@@ -1,9 +1,26 @@
-int LMOTOR = 0;
-int RMOTOR = 3;
-int LSERVO = 2;
-int RSERVO = 0;
-int NBEACON= 10;
 
+//Port Declarations
+const int LMOTOR = 0;
+const int RMOTOR = 3;
+const int LSERVO = 2;
+const int RSERVO = 0;
+const int NBEACON= 4;
+const int EBEACON= 5;
+const int SBEACON= 6;
+const int WBEACON= 7;
+
+//Value Declarations
+int beaconVal = 100;
+
+int rightArmDown = 1100;
+int leftArmDown = 1200;
+int rightArmUp = 0;
+int leftArmUp = 1024;
+
+int defaultSpeed = 50;
+int defaultTime = 50;
+
+//Command Declarations
 void foward();
 void turnLeft();
 void turnRight();
@@ -11,7 +28,7 @@ void turnAround();
 void manipulate();
 void findBeacon();
 
-/////////////////////////
+//////////MAIN///////////
 
 int main(){
 	while(1){
@@ -22,45 +39,45 @@ int main(){
 /////////////////////////
 
 void forward(){
-	motor(LMOTOR, 50);
-	motor(RMOTOR, 50);
-	sleep(30);
+	motor(LMOTOR, defaultSpeed);
+	motor(RMOTOR, defaultSpeed);
+	sleep(defaultTime / 2);
 }
 
 void turnLeft(){
-	motor(LMOTOR, -50);
-	motor(RMOTOR, 50);
-	sleep(50);
+	motor(LMOTOR, -defaultSpeed);
+	motor(RMOTOR, defaultSpeed);
+	sleep(defaultTime);
 }
 
 void turnRight(){
-	motor(LMOTOR, 50);
-	motor(RMOTOR, -50);
-	sleep(50);
+	motor(LMOTOR, defaultSpeed);
+	motor(RMOTOR, -defaultSpeed);
+	sleep(defaultTime);
 }
 
 void turnAround(){
-	motor(LMOTOR, -50);
-	motor(RMOTOR, 50);
-	sleep(100);
+	motor(LMOTOR, -defaultSpeed);
+	motor(RMOTOR, defaultSpeed);
+	sleep(defaultTime * 2);
 }
 
 void manipulate(){
-	set_servo_position(LSERVO, 1200);
-	set_set_position(RSERVO, 1100);
+	set_servo_position(LSERVO, leftArmDown);
+	set_set_position(RSERVO, rightArmDown);
 }
 
 void findBeacon(){
-	if(NBEACON < 100){
+	if(NBEACON < beaconVal){
 		forward();
 	}
-	else if(EBEACON < 100){
+	else if(EBEACON < beaconVal){
 		turnRight();
 	}
-	else if(WBEACON < 100){
+	else if(WBEACON < beaconVal){
 		turnLeft();
 	}
-	else if(SBEACON < 100{
+	else if(SBEACON < beaconVal){
 		turnAround();
 	}
 	else{
