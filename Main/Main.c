5f756eabae8625ffb2e9a54fd36172c3f2b9dfd7
Squidswell <betidswell@vassar.edu>
2016-11-17 17:14:23 -0500

    End of Day 11/17

--- Main/Main.c
+++ Main/Main.c
@@ -4,14 +4,18 @@ const int RMOTOR = 3;
 const int LSERVO = 0;
 const int RSERVO = 2;
 const int CAMSERVO = 1;
-const int FRONT_BUMP= 15;
-const int LEFT_IR= 2;
-const int RIGHT_IR= 1;
+const int FRONT_BUMP = 15;
+const int LEFT_IR = 2;
+const int RIGHT_IR = 1;
+const int RIGHT_LINE = 12;
+const int LEFT_LINE = 8;
 
 
 //Camera Stuff
 const int CAMERA_BLUE = 0;
+const int CAMERA_GREEN = 1;
 
+int block_count;
 int block_position_x;
 int block_position_y;
 int block_width;
@@ -27,13 +31,13 @@ int leftArmDown = 1200;
 int rightArmUp = 0;
 int leftArmUp = 2040;
 
-int camUp = 0;
+int camUp = 500;
 int camDown = 900;
 
 int defaultSpeed = 60;
 int defaultTime = 300;
 
-int threshIR = 250;
+int threshIR = 300;
 
 //Command Declarations
 void forward();
@@ -46,9 +50,13 @@ void handsUp();
 void backUp();
 int hitWall();
 int hitBlock();
+int inNest();
+int lookBlock();
 void roam();
 void collectBlock();
 void stop();
+void seekNest();
+void trackBlock();
 
 //Define boolean constants
 const int TRUE = 1;
@@ -60,11 +68,17 @@ int main(){
 	camera_open();
 	set_servo_position(CAMSERVO, camUp);
 	int hasBlock = FALSE;
+	int nest = FALSE;
+	int seekBlock = FALSE;
 	
     while(1){
 		if(hasBlock == FALSE){
 			handsUp();
-			if(hitWall() == TRUE){
+			if(lookBlock()==TRUE && hitBlock() == FALSE){
+				trackBlock();
+				forward();
+			}
+			else if(hitWall() == TRUE){
 				printf("hitWall \n");
 				backUp();
 				turnAround();
@@ -80,7 +94,16 @@ int main(){
 			}
 		}
 		else if(hasBlock == TRUE){
-			roam(0.75);
+			//seekNest();
+			//printf("found nest!");
+			roam(1.5);
+			//if(inNest()){
+			//	nest = TRUE;
+			//	hasBlock = FALSE;
+			//}
+		}
+		else if(nest == TRUE){
+		stop();
 		}
 	}
 }
@@ -108,13 +131,13 @@ int hitWall(){
     }
 }
 
-//return tru of false if we have hit a block
+//return true or false if we have hit a block
 int hitBlock(){
 	int left_ir;
 	int right_ir;
     int front_bump;
 	
-	int blockThreshold_ir = 300;
+	int blockThreshold_ir = 350;
 	
 	left_ir = analog_et(LEFT_IR);
 	right_ir = analog_et(RIGHT_IR);
@@ -133,15 +156,17 @@ int hitBlock(){
 
 ///////////ROAM//////////
 
+/* mod is a number passed  to the roam function to modify the reaction time and speed if the robot. 
+*/
 void roam(mod){
 	int rightIRValue = analog_et(RIGHT_IR);
 	int leftIRValue = analog_et(LEFT_IR);
 	
-	int rmSpeed = defaultSpeed + defaultSpeed * (1/mod) * (rightIRValue / threshIR);
-	int lmSpeed = defaultSpeed + defaultSpeed * (1/mod) * (leftIRValue / threshIR);
+	int rmSpeed = defaultSpeed + defaultSpeed * (1/mod) * (rightIRValue / threshIR) * 0.75;
+	int lmSpeed = defaultSpeed + defaultSpeed * (1/mod) * (leftIRValue / threshIR) * 0.75;
 	
-	motor(LMOTOR, lmSpeed * mod);
-	motor(RMOTOR, rmSpeed * mod);
+	motor(LMOTOR, (lmSpeed * mod) - 25);
+	motor(RMOTOR, (rmSpeed * mod) - 25);
 }
 
 /////////BASIC MOVEMENT/////////
@@ -203,12 +228,13 @@ int checkBlock(){
 	block_height = get_object_bbox(CAMERA_BLUE, 0).height;
 	block_area = block_width * block_height;
 	
-	if(block_area > 50){
+	if(block_area > 100){
 		return(TRUE);
 	}
 	else{
 		return(FALSE);
 	}
+	set_servo_position(CAMSERVO, camUp);
 }
 
 
@@ -233,7 +259,87 @@ void collectBlock(){
 				motor(LMOTOR, defaultSpeed);
 				msleep(50);
 			}
+			//motor(RMOTOR, -defaultSpeed);
+			//motor(LMOTOR, defaultSpeed);
 		}
 	printf("found block \n");
 	handsDown();
+	set_servo_position(CAMSERVO, camUp);
 }
+
+int lookBlock(){
+	camera_update();
+	int block_count = get_object_count(CAMERA_BLUE);
+	
+	if (block_count >= 1){
+			block_width = get_object_bbox(CAMERA_BLUE, 0).width;
+			block_height = get_object_bbox(CAMERA_BLUE, 0).height;
+			block_area = block_width * block_height;
+		if(block_area > 150){
+			return TRUE;
+		}
+	}
+	else{return FALSE;}
+	printf("look block \n");
+}
+
+void trackBlock(){
+	camera_update();
+	block_position_x = get_object_center(CAMERA_BLUE, 0).x;
+	block_position_y = get_object_center(CAMERA_BLUE, 0).y;
+	
+	while(block_position_x < 30 || block_position_x > 130){
+		printf("tracking \n");
+		camera_update();
+		block_position_x = get_object_center(CAMERA_BLUE, 0).x;
+			
+		if(block_position_x < 60){
+			motor(RMOTOR, defaultSpeed);
+			motor(LMOTOR, -defaultSpeed);
+			msleep(50);
+		}
+		else if(block_position_x > defaultSpeed){
+			motor(RMOTOR, -defaultSpeed);
+			motor(LMOTOR, defaultSpeed);
+			msleep(50);
+		}
+	}	
+	
+}
+
+void seekNest(){
+	camera_update();
+	int nest_position_x = get_object_center(CAMERA_GREEN, 0).x;
+	int nest_position_y = get_object_center(CAMERA_GREEN, 0).y;
+	
+	while(nest_position_x < 60 || nest_position_x > 100){
+		printf("collecting \n");
+		camera_update();
+		nest_position_x = get_object_center(CAMERA_GREEN, 0).x;
+		
+		if(nest_position_x < 60){
+			motor(RMOTOR, defaultSpeed);
+			motor(LMOTOR, -defaultSpeed);
+			msleep(50);
+		}
+		else if(nest_position_x > defaultSpeed){
+			motor(RMOTOR, -defaultSpeed);
+			motor(LMOTOR, defaultSpeed);
+			msleep(50);
+		}
+	}
+}
+
+int inNest(){
+	int rline = analog_et(RIGHT_LINE);
+	int lline = analog_et(LEFT_LINE);
+	
+	if(rline == TRUE && lline==TRUE){
+		msleep(500);
+		if(rline == TRUE && lline==TRUE){
+		return TRUE;
+		}
+	}
+	else{return FALSE;}
+}
+
