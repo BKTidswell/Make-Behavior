1f42e5535451c1f550cf4905a092176a5bf2b99a
Squidswell <betidswell@vassar.edu>
2016-11-17 15:01:17 -0500

    11/17 3:01

--- Main/Main.c
+++ Main/Main.c
@@ -3,6 +3,7 @@ const int LMOTOR = 0;
 const int RMOTOR = 3;
 const int LSERVO = 0;
 const int RSERVO = 2;
+const int CAMSERVO = 1;
 const int FRONT_BUMP= 15;
 const int LEFT_IR= 2;
 const int RIGHT_IR= 1;
@@ -15,6 +16,7 @@ int block_position_x;
 int block_position_y;
 int block_width;
 int block_height;
+int block_area; 
 int block_error_y;
 int block_error_x;
 int targets;
@@ -25,7 +27,10 @@ int leftArmDown = 1200;
 int rightArmUp = 0;
 int leftArmUp = 2040;
 
-int defaultSpeed = 75;
+int camUp = 0;
+int camDown = 900;
+
+int defaultSpeed = 60;
 int defaultTime = 300;
 
 int threshIR = 250;
@@ -42,6 +47,8 @@ void backUp();
 int hitWall();
 int hitBlock();
 void roam();
+void collectBlock();
+void stop();
 
 //Define boolean constants
 const int TRUE = 1;
@@ -49,29 +56,31 @@ const int FALSE = 0;
 
 //main loop
 int main(){
+	enable_servos();
 	camera_open();
+	set_servo_position(CAMSERVO, camUp);
 	int hasBlock = FALSE;
 	
     while(1){
 		if(hasBlock == FALSE){
 			handsUp();
-			if(hitWall()== TRUE){
+			if(hitWall() == TRUE){
 				printf("hitWall \n");
 				backUp();
 				turnAround();
 			}
 			else if(hitBlock() == TRUE){
 				printf("hitBlock \n");
+				stop();
 				hasBlock = TRUE;
 				collectBlock();
 			}
 			else{
-				printf("else \n");
 				roam(1);
 			}
 		}
 		else if(hasBlock == TRUE){
-			roam(2);
+			roam(0.75);
 		}
 	}
 }
@@ -79,7 +88,6 @@ int main(){
 
 //return true or false telling whether robot has hit the wall
 int hitWall(){
-    
 	int left_ir;
 	int right_ir;
     int front_bump;
@@ -105,7 +113,7 @@ int hitBlock(){
 	int left_ir;
 	int right_ir;
     int front_bump;
-    
+	
 	int blockThreshold_ir = 300;
 	
 	left_ir = analog_et(LEFT_IR);
@@ -113,7 +121,8 @@ int hitBlock(){
 	front_bump = digital(FRONT_BUMP);
 	
     if(front_bump == TRUE){
-		if(left_ir < blockThreshold_ir || right_ir < blockThreshold_ir){
+		if(left_ir < blockThreshold_ir && right_ir < blockThreshold_ir && 
+			checkBlock() == TRUE){
         return TRUE;
 		}
     }
@@ -128,11 +137,11 @@ void roam(mod){
 	int rightIRValue = analog_et(RIGHT_IR);
 	int leftIRValue = analog_et(LEFT_IR);
 	
-	int rmSpeed = defaultSpeed + defaultSpeed * mod * (rightIRValue / threshIR);
-	int lmSpeed = defaultSpeed + defaultSpeed * mod * (leftIRValue / threshIR);
+	int rmSpeed = defaultSpeed + defaultSpeed * (1/mod) * (rightIRValue / threshIR);
+	int lmSpeed = defaultSpeed + defaultSpeed * (1/mod) * (leftIRValue / threshIR);
 	
-	motor(LMOTOR, lmSpeed / mod);
-	motor(RMOTOR, rmSpeed / mod);
+	motor(LMOTOR, lmSpeed * mod);
+	motor(RMOTOR, rmSpeed * mod);
 }
 
 /////////BASIC MOVEMENT/////////
@@ -175,39 +184,56 @@ void stop(){
 ///////ARM CONTROL//////////
 
 void handsDown(){
-	enable_servos();
 	set_servo_position(LSERVO, leftArmDown);
 	set_servo_position(RSERVO, rightArmDown);
 }
 
 void handsUp(){
-	enable_servos();
 	set_servo_position(LSERVO, leftArmUp);
 	set_servo_position(RSERVO, rightArmUp);
 }
 
 //////CAMERA STUFF/////////
 
+int checkBlock(){
+	set_servo_position(CAMSERVO, camDown);
+	msleep(100);
+	camera_update();
+	block_width = get_object_bbox(CAMERA_BLUE, 0).width;
+	block_height = get_object_bbox(CAMERA_BLUE, 0).height;
+	block_area = block_width * block_height;
+	
+	if(block_area > 50){
+		return(TRUE);
+	}
+	else{
+		return(FALSE);
+	}
+}
+
+
 void collectBlock(){
+	set_servo_position(CAMSERVO, camDown);
 	camera_update();
 	block_position_x = get_object_center(CAMERA_BLUE, 0).x;
 	block_position_y = get_object_center(CAMERA_BLUE, 0).y;
 	
 	while(block_position_x < 60 || block_position_x > 100){
+			printf("collecting \n");
 			camera_update();
 			block_position_x = get_object_center(CAMERA_BLUE, 0).x;
 			
 			if(block_position_x < 60){
-				motor(RMOTOR, 50);
-				motor(LMOTOR, -50);
+				motor(RMOTOR, defaultSpeed);
+				motor(LMOTOR, -defaultSpeed);
 				msleep(50);
 			}
-			else if(block_position_x > 100){
-				motor(RMOTOR, -50);
-				motor(LMOTOR, 50);
+			else if(block_position_x > defaultSpeed){
+				motor(RMOTOR, -defaultSpeed);
+				motor(LMOTOR, defaultSpeed);
 				msleep(50);
 			}
 		}
-		
+	printf("found block \n");
 	handsDown();
 }
